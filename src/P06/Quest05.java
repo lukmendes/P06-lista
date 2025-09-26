04a88a85cd11060aaa833d23e689358969f16779
Lucas Mendes <lms252006@gmail.com>
2025-09-26 16:05:22 -0300

    revisões 2

--- src/P06/Quest05.java
+++ src/P06/Quest05.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Quest05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int i = 1, result = 0;
+        int i = 1, result;
 
         System.out.println("Digite um número inteiro: ");
         int number = scan.nextInt();
 
         System.out.println("Números de termos de entrada: " + number);
-        while (number >= i) {
+        do {
             result = i * i * i;
             System.out.printf("O número é: %d e o cubo de %d é: %d \n", i, i, result);
             i++;
-        }
+        } while (number >= i);
 
         scan.close();
     }
