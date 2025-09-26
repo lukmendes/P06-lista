764fc918f0285eb6d1855452f74c879c4b52b552
Lucas Mendes <lms252006@gmail.com>
2025-09-26 16:05:07 -0300

    sexta questão resolvida

--- src/P06/Quest06.java
+++ src/P06/Quest06.java
@@ -1,4 +1,22 @@
 package P06;
 
+import java.util.Scanner;
+
 public class Quest06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 1, result;
+
+        System.out.println("Digite um número inteiro: ");
+        int number = scan.nextInt();
+
+        System.out.println("A tabuada de " + number + " é igual a: ");
+        while (10 >= i) {
+            result = number * i;
+            System.out.printf("%d x %d = %d \n", number, i, result);
+            i++;
+        }
+
+        scan.close();
+    }
 }
