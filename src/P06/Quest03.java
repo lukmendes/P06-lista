964e5ba69586d07fc260045dbc6154f9af4b215b
Lucas Mendes <lms252006@gmail.com>
2025-09-26 14:47:59 -0300

    terceira questão resolvida

--- src/P06/Quest03.java
+++ src/P06/Quest03.java
@@ -1,4 +1,25 @@
 package P06;
 
+import java.util.Scanner;
+
 public class Quest03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberC = 0;
+
+        System.out.println("Digite o primeiro número: ");
+        int numberA = scan.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int numberB = scan.nextInt();
+
+        if (numberA == numberB) {
+            numberC = numberA + numberB;
+            System.out.println("Seu resultado é: " + numberC);
+        } else {
+            numberC = numberA * numberB;
+            System.out.println("Seu resultado é: " + numberC);
+        }
+
+        scan.close();
+    }
 }
