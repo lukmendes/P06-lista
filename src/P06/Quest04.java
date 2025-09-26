d8120bce5e7cf698185c10c3f5e8a32dbf3acbdd
Lucas Mendes <lms252006@gmail.com>
2025-09-26 15:17:21 -0300

    quarta questão resolvida

--- src/P06/Quest04.java
+++ src/P06/Quest04.java
@@ -1,4 +1,22 @@
 package P06;
 
+import java.util.Scanner;
+
 public class Quest04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a sua altura: ");
+        double height = scan.nextDouble();
+        System.out.println("Digite o seu gênero: ");
+        String gender = scan.next();
+
+        if (gender.equals("M")) {
+            System.out.println("Seu peso ideal é: " + ((72.7 * height) - 58));
+        } else {
+            System.out.println("Seu peso ideal é: " + ((62.1 * height) - 44.7));
+        }
+
+        scan.close();
+    }
 }
