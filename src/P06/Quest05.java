e2bed670a042474fcf12d935fe19441ff0efea6b
Lucas Mendes <lms252006@gmail.com>
2025-09-26 15:42:55 -0300

    quinta questão resolvida

--- src/P06/Quest05.java
+++ src/P06/Quest05.java
@@ -1,4 +1,22 @@
 package P06;
 
+import java.util.Scanner;
+
 public class Quest05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 1, result = 0;
+
+        System.out.println("Digite um número inteiro: ");
+        int number = scan.nextInt();
+
+        System.out.println("Números de termos de entrada: " + number);
+        while (number >= i) {
+            result = i * i * i;
+            System.out.printf("O número é: %d e o cubo de %d é: %d \n", i, i, result);
+            i++;
+        }
+
+        scan.close();
+    }
 }
