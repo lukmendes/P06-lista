6b425ae337dcba0774ebe3322dec44bb634c6749
Lucas Mendes <lms252006@gmail.com>
2025-09-26 14:36:52 -0300

    segunda questão resolvida

--- src/P06/Quest02.java
+++ src/P06/Quest02.java
@@ -1,4 +1,18 @@
 package P06;
 
+import java.util.Scanner;
+
 public class Quest02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número qualquer: ");
+        int number = scan.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Este número é par.");
+        } else {
+            System.out.println("Este número é impar.");
+        }
+    }
 }
